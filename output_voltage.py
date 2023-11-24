e88282ba4a9373651bbbe71a2e83f601af3c8c36
Marek Newton <marek.s.newton@gmail.com>
2023-11-23 23:32:10 -0700

    switch to time based

--- output_voltage.py
+++ output_voltage.py
@@ -5,7 +5,7 @@ FREQ = 100 # Hertz
 VOLTAGE = 5 # V
 
 ACCURACY = 0.01
-ADC_STEPS = 4096
+ADC_STEPS = 2896
 ADC_VOLTAGE_REFERENCE = 2.5 # V
 LSB = ADC_VOLTAGE_REFERENCE/ADC_STEPS # V
 
@@ -19,7 +19,9 @@ MAXIMUM_ADC_VOLTAGE = ADC_VOLTAGE_REFERENCE - MINIMUM_ADC_VOLTAGE
 DIGITS_TO_PRINT = 3
 
 def get_capacitance(Vr, resistor_value):
-    return sqrt(1/((VOLTAGE**2/Vr**2) - 1))/(2*pi*FREQ*resistor_value)
+    # sqrt(1/((VOLTAGE**2/Vr**2) - 1))/(2*pi*FREQ*resistor_value)
+    w = 2*pi*FREQ
+    return ((VOLTAGE/Vr)**2 - 1)/(w**2*resistor_value**2)
 
 
 def print_capacitance(capacitance):
@@ -28,15 +30,15 @@ def print_capacitance(capacitance):
         print(str(round(capacitance_microfarads, DIGITS_TO_PRINT)) + 'uF')
     elif(capacitance < 1e-6 and capacitance >= 1e-9):
         capacitance_nanofarads = capacitance/NANO
-        print(str(round(capacitance_nanofarads, DIGITS_TO_PRINT))+ 'nF')
+        print(str(round(capacitance_nanofarads, DIGITS_TO_PRINT)) + 'nF')
     elif(capacitance < 1e-9):
         capacitance_picofarads = capacitance/PICO
         print(str(round(capacitance_picofarads, DIGITS_TO_PRINT)) + 'pF')
 
 
 def print_range():
-    for i in range(0, 7):
-        resistor_value = 10**i*7
+    for i in range(1, 5):
+        resistor_value = 10**i*9
 
         max_first_digit = int(str(get_capacitance(MAXIMUM_ADC_VOLTAGE, resistor_value))[0])
         min_first_digit = int(str(get_capacitance(MINIMUM_ADC_VOLTAGE, resistor_value))[0])
@@ -46,13 +48,11 @@ def print_range():
             print_capacitance(get_capacitance(MAXIMUM_ADC_VOLTAGE, resistor_value))
             print_capacitance(get_capacitance(MINIMUM_ADC_VOLTAGE, resistor_value))
 
-        
         if(max_first_digit == 2 and min_first_digit == 1):
             print("Resistance: " + str(resistor_value) + " ohms")
             print_capacitance(get_capacitance(MAXIMUM_ADC_VOLTAGE, resistor_value))
             print_capacitance(get_capacitance(MINIMUM_ADC_VOLTAGE, resistor_value))
 
-        
         if(max_first_digit == 1 and min_first_digit == 2):
             print("Resistance: " + str(resistor_value) + " ohms")
             print_capacitance(get_capacitance(MAXIMUM_ADC_VOLTAGE, resistor_value))
