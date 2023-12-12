fb15501638c73f3c64d4258f8d068690cab98da0
Marek Newton <marek.s.newton@gmail.com>
2023-12-12 00:37:05 -0700

    crashed

--- firmware/src/ADC.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADC_H
-#define ADC_H
-
-void setup_ADC(void);
-void start_ADC_conversion(void);
-
-#endif
--- firmware/src/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#define COMPONENT_VOLTAGE_FILTER_LENGTH 100
-
-void adc_complete(int value);
-
-#endif
