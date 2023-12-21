f80303e4853c88aa2c8aa4a7a95fea4fa4608702
Marek Newton <marek.s.newton@gmail.com>
2023-12-20 19:50:41 -0700

    finished algorithm

--- algorithm/measure.py
+++ algorithm/measure.py
@@ -2,7 +2,7 @@ from math import sqrt, pi
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from scipy.signal import resample, remez
+from scipy.signal import resample 
 
 SAMPLE_RATE = 10e6
 SAMPLE_PERIOD = 1/SAMPLE_RATE
@@ -17,10 +17,10 @@ class ValueFinder:
     HIGH_PASS_LENGTH = 10
     HIGH_PASS_TAU = 1/(2*pi*HIGH_PASS_CUTOFF)
     
-    def __init__(self, voltage_samples, current_samples, times):
+    def __init__(self, voltage_samples, current_samples, peak_detect_mode=False):
         self.voltage_samples = voltage_samples
         self.current_samples = current_samples
-        self.times = times
+        self.peak_detect_mode = peak_detect_mode
     
     def input_voltage(self, output_voltage):
         I = output_voltage/(self.R_TOP + self.R_BOTTOM)
@@ -49,12 +49,35 @@ class ValueFinder:
             sum_square += sample**2
         return sqrt(sum_square/len(sampled_amplitude)) 
 
+    def RMS_peak(self, sampled_amplitude):
+        return self.RMS(sampled_amplitude)*sqrt(2)
+
+    def peak_detector(self, signal):
+        max_point = 0
+        for point in signal:
+            if(point > max_point):
+                max_point = point
+        return max_point
+
     def measured_capacitance(self):
         UNDO_GAIN = 1/self.TRANSIMPEDANCE_GAIN
-        mag_voltage = self.input_voltage(self.RMS(self.DC_Blocker(self.voltage_samples))*sqrt(2))
-        mag_current = self.input_voltage(self.RMS(self.DC_Blocker(self.current_samples))*sqrt(2))*UNDO_GAIN
-        return self.capacitance(mag_voltage/mag_current)
+        if(not self.peak_detect_mode):
+            mag_voltage = self.input_voltage(self.RMS_peak(self.DC_Blocker(self.voltage_samples)))
+            mag_current = self.input_voltage(self.RMS_peak(self.DC_Blocker(self.current_samples)))*UNDO_GAIN
+            return self.capacitance(mag_voltage/mag_current)
+        else:
+            mag_voltage = self.input_voltage(self.peak_detector(self.DC_Blocker(self.voltage_samples)))
+            mag_current = self.input_voltage(self.peak_detector(self.DC_Blocker(self.current_samples))*UNDO_GAIN)
+            return self.capacitance(mag_voltage/mag_current)
        
+def is_evenly_spaced(data):
+    last_point = 0
+    space = None
+    for point in data:
+       new_space = point - last_point
+       if(space is not None and not np.isclose(new_space, space)): return False
+       space = new_space
+    return True
 
 voltage_data = np.array(pd.read_csv('voltage.txt', sep = '\t', header=None))
 current_data = np.array(pd.read_csv('current.txt', sep = '\t', header=None))
@@ -65,27 +88,37 @@ voltage_data_voltage = voltage_data[:,1]
 current_data_time = current_data[:,0]
 current_data_voltage = current_data[:,1]
 
+print(is_evenly_spaced(voltage_data_time))
+print(is_evenly_spaced(current_data_time))
+
 time_length = voltage_data_time[voltage_data_time.size - 1] - voltage_data_time[0]
 ADC_sample_count = (int)(time_length/SAMPLE_PERIOD)
 
-ADC_voltage_samples = resample(voltage_data_voltage, ADC_sample_count)
-ADC_current_samples = resample(current_data_voltage, ADC_sample_count)
+ADC_voltage_samples, _ = resample(voltage_data_voltage, ADC_sample_count, t=voltage_data_time)
+ADC_current_samples, _ = resample(current_data_voltage, ADC_sample_count, t=current_data_time)
 times = np.linspace(0, time_length, num=ADC_sample_count)
 
-capacitance_value_finder = ValueFinder(ADC_voltage_samples, ADC_current_samples, times)
-ac_voltage = capacitance_value_finder.DC_Blocker(ADC_voltage_samples)
-ac_current = capacitance_value_finder.DC_Blocker(ADC_current_samples)
+value_finder_resampled = ValueFinder(ADC_voltage_samples, ADC_current_samples)
+ac_voltage = value_finder_resampled.DC_Blocker(ADC_voltage_samples)
+ac_current = value_finder_resampled.DC_Blocker(ADC_current_samples)
 
-print(capacitance_value_finder.measured_capacitance())
-print(capacitance_value_finder.mean(ADC_voltage_samples))
-print(capacitance_value_finder.mean(ADC_current_samples))
+print(value_finder_resampled.measured_capacitance())
+print(value_finder_resampled.mean(ADC_voltage_samples))
+print(value_finder_resampled.mean(ADC_current_samples))
+
+value_finder_original_data = ValueFinder(voltage_data_voltage, current_data_voltage, peak_detect_mode=True)
+print(value_finder_original_data.measured_capacitance())
 
 plt.plot(times, ac_current)
 plt.plot(times, ac_voltage)
 plt.show()
 
+plt.title('Voltage Resampling')
 plt.plot(voltage_data_time, voltage_data_voltage)
+plt.scatter(times, ADC_voltage_samples, s=2, color='red')
 plt.show()
 
+plt.title('Current Resampling')
 plt.plot(current_data_time, current_data_voltage)
+plt.scatter(times, ADC_current_samples, s=2, color='red')
 plt.show()
