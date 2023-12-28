e282219b0e7aa319f1f36459930ffdfa694a9c87
Marek Newton <marek.s.newton@gmail.com>
2023-12-28 14:58:44 -0700

    firmware talking

--- firmware/main.c
+++ firmware/main.c
@@ -1,11 +1,28 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 
 int main(void)
 {
-    while(1)
-    {
+	stdio_init_all();
+	const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
-    }
+	gpio_init(LED_PIN);
 
-    return 1;
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	gpio_put(LED_PIN, 1);
+	printf("Hello UART\n");
+
+	while(1)
+	{
+		gpio_put(LED_PIN, 0);
+		printf("GOD\n");
+		sleep_ms(1000);
+
+
+		gpio_put(LED_PIN, 1);
+		printf("LED OFF\n");
+		sleep_ms(1000);
+	}
+
+	return 1;
 }
